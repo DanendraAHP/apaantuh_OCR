1dc3adddd857d6cf096186b9b4e913162348a637
DanendraAHP <danendra6118@gmail.com>
2023-04-09 10:27:46 +0700

    add crnn

--- src/config.py
+++ src/config.py
@@ -10,17 +10,24 @@ DATASET_CONFIG = {
     'SCALE' : 1./255,
     'OFFSET' : 0.0,
     'KFOLD_SPLIT' : 5,
+    'AUGMENT_DATA' : True
 }
 
 MODEL_CONFIG = {
-    'MODEL_TYPE' : 'mobilenet',#baseline/mobilenet/inception,
+    'MODEL_TYPE' : 'inception',#baseline/mobilenet/efficientnet,
     'PREPROCESS_LAYER' : {
         'mobilenet' : tf.keras.applications.mobilenet_v2.preprocess_input,
+        'efficientnet' : tf.keras.applications.efficientnet_v2.preprocess_input,
+        #cant be used because mnist only have 28x28 meanwhile the model need at least 150x150
+        'inception' : tf.keras.applications.inception_v3.preprocess_input
     },
     'BASE_MODEL' : {
-        'mobilenet' : tf.keras.applications.MobileNetV2(include_top=False, weights='imagenet')
+        'mobilenet' : tf.keras.applications.MobileNetV2(include_top=False, weights='imagenet'),
+        'efficientnet' : tf.keras.applications.efficientnet_v2.EfficientNetV2M(include_top=False, weights='imagenet'),
+        #cant be used because mnist only have 28x28 meanwhile the model need at least 150x150
+        'inception' : tf.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet')
     },
-    'MODEL_FOLDER' : 'models/mobilenet',
+    'MODEL_FOLDER' : 'models/inception',
     'FINE_TUNE_AT' : 50,
     'LEARNING_RATE' : 0.0001,
     'EPOCHS' : 1000,
--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -33,9 +33,18 @@ def load_az_dataset(datasetPath):
 
 #combining data for training models
 def combining_dataset(az_filepath):
+    combine_start = time.time()
     #combining dataset
+    #load mnist data
+    start = time.time()
     mnist_data, mnist_label = load_mnist_dataset()
+    end = time.time()
+    print(f'[INFO] load mnist data took {end-start} second')
+    #load az data
+    start = time.time()
     az_data, az_labels = load_az_dataset(az_filepath)
+    end = time.time()
+    print(f'[INFO] load az data took {end-start} second')
     az_labels+=10
     data = np.vstack([az_data, mnist_data])
     labels = np.hstack([az_labels, mnist_label])
@@ -46,6 +55,8 @@ def combining_dataset(az_filepath):
     y_test = tf.one_hot(y_test, num_classess)
     X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1))
     X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1))
+    combine_end = time.time()
+    print(f'[INFO] combining dataset took {combine_end-combine_start} seconds')
     return X_train, X_test, y_train, y_test
 
 class Dataset:
@@ -60,28 +71,30 @@ class Dataset:
     self.offset = DATASET_CONFIG['OFFSET']
     self.num_classess = DATASET_CONFIG['NUM_CLASS']
     self.kfold_split = DATASET_CONFIG['KFOLD_SPLIT']
+    self.augment = DATASET_CONFIG['AUGMENT_DATA']
     #keras layer for preprocessing and augmentation
     self.resize_and_rescale = tf.keras.Sequential([
       tf.keras.layers.Resizing(self.img_height, self.img_width),
       tf.keras.layers.Rescaling(scale=self.scale, offset=self.offset)
     ])
-    # self.data_augmentation = tf.keras.Sequential([
-    #   tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-    #   tf.keras.layers.RandomRotation(0.2),
-    # ])
+    self.data_augmentation = tf.keras.Sequential([
+      tf.keras.layers.RandomHeight(0.1),
+      tf.keras.layers.RandomRotation(0.1),
+      tf.keras.layers.RandomWidth(0.1)
+    ])
   def preprocess(self, img, label):
     img = self.resize_and_rescale(img)
     img = tf.image.grayscale_to_rgb(img)
     return img,label
-  def preprocess_augment(self, ds):
+  def preprocess_augment(self, ds, augment=False):
     # Resize and rescale all datasets.
     ds = ds.map(self.preprocess)
     # Batch all datasets.
     ds = ds.batch(self.batch_size)
     # Use data augmentation only on the training set.
-    # if augment:
-    #   ds = ds.map(lambda x, y: (self.data_augmentation(x, training=True), y), 
-    #               num_parallel_calls=self.autotune)
+    if augment:
+      ds = ds.map(lambda x, y: (self.data_augmentation(x, training=True), y), 
+                  num_parallel_calls=self.autotune)
     # Use buffered prefetching on all datasets.
     return ds.prefetch(buffer_size=self.autotune)
 
@@ -91,8 +104,7 @@ class Dataset:
     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
     #preprocess and augment data
-    #train_dataset = self.preprocess_augment(train_dataset, augment=True)
-    train_dataset = self.preprocess_augment(train_dataset)
+    train_dataset = self.preprocess_augment(train_dataset, self.augment)
     val_dataset = self.preprocess_augment(val_dataset)
     return train_dataset, val_dataset
   #get combined data and get index from KFOLD
